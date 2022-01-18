66d43fa78bfb09f4b9bf8011636205cc7340a081
Bernardo Santos <2190723@my.ipleiria.pt>
2022-01-18 15:13:36 +0000

    action recentes com imagem created

--- leirisymphony-api/controllers/ProdutosController.php
+++ leirisymphony-api/controllers/ProdutosController.php
@@ -18,6 +18,28 @@ class ProdutosController extends ActiveController
         return $produtos;
     }
 
+    public function actionRecentesComImagem(){
+        $produtos = Produtos::find()->orderBy(['id' => SORT_DESC])->limit(4)->all();
+        
+        $produtosComImagem = array();
+        foreach($produtos as $produto){
+            $myObj = new \stdClass();
+            $imagens = Imagens::find()->where(['idproduto' => $produto->id])->all();
+            $myObj->id = $produto->id;
+            $myObj->idsubcategoria = $produto->idsubcategoria;
+            $myObj->idmarca = $produto->idmarca;
+            $myObj->nome = $produto->nome;
+            $myObj->descricao = $produto->descricao;
+            $myObj->usado = $produto->usado;
+            $myObj->preco = $produto->preco;
+            $myObj->stock = $produto->stock;
+            $myObj->imagemPrincipal = base64_encode(file_get_contents(\Yii::getAlias('@imageurl').'/'.$imagens[0]->nome));
+            array_push($produtosComImagem, $myObj);
+        }
+
+        return $produtosComImagem;
+    }
+
     public function actionPorCategoria($categoriaid){
         $subcategorias = Subcategorias::find()->select('id')->where(['idcategoria' => $categoriaid])->all();
         $produtos = Produtos::find()->innerJoinWith('idsubcategoria0')->where(['idcategoria' => $categoriaid])->all();
